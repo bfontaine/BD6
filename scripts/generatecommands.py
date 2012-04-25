884533a080729e9c1073d5486f7edb3470eba521
Baptiste Fontaine <batifon@yahoo.fr>
2012-04-25 14:45:40 +0200

    debut du script de generation des commandes ajouté

---
+++ scripts/generatecommands.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3.2
+# -*- coding: UTF-8 -*-
+
+OUTPUT_FILE = 'commands.sql'
+
+# génère 250 commandes de façon aléatoire
+#
+# - 180 commandes complètes (expédiées)
+# - 20 commandes à moitié expédiées
+# - 50 commandes non expédiées
+#
+
+#TODO
