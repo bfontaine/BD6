645f1089545c8b7a50c80eb04285fa53658236b1
Baptiste Fontaine <batifon@yahoo.fr>
2012-04-25 14:29:13 +0200

    ajout de filldb pour automatiser l'insertion des données de base dans la BDD

---
+++ scripts/filldb.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo -n 'CSV->JSON...'
+./csv2json.py && echo 'ok' || echo 'fail! '
+echo -n 'JSON->SQL...'
+./json2sql.py && echo 'ok' || echo 'fail! '
+echo -n 'exécution des requêtes...'
+psql bd6 -f ./data.sql >/dev/null 2>./filldb.log && echo 'ok' || echo 'fail! '
+
+if [ -s ./filldb.log ]; then
+    echo 'Des erreurs sont survenues, consultez le fichier "filldb.log" pour'
+    echo "plus d'informations."
+else
+    rm -f ./filldb.log
+fi
