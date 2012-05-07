781237d35d247809ec17878989ed72c9df27a92d
david <galichet.david@gmail.com>
2012-05-07 16:15:09 +0200

    programmation java

--- java/fenetreco.java
+++ java/fenetreco.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Connexion extends JFrame{
+public class fenetre_co extends JFrame{
     requete re;
     JTextField login;
     JTextField mdp;
