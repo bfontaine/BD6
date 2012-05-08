e7f20c157e762a107b395e60ed57476603dce27e
Baptiste Fontaine <batifon@yahoo.fr>
2012-05-08 15:15:02 +0200

    programme de génération des commandes aléatoires ajouté en Java (pas encore écrit)

---
+++ java/GenerateCommands.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.sql.*;
+
+public class GenerateCommands {
+
+    public static void main (String[] args) {
+        ConnexionBDD co = new ConnexionBDD("baptiste", "baptiste");
+    }
+}
