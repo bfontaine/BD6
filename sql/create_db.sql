d6d99bfe8dd9b4f71f47b3e81b5e094d88516366
Baptiste Fontaine <batifon@yahoo.fr>
2012-05-10 20:31:00 +0200

    quelques contraintes ajoutées à la structure de la BDD

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -5,8 +5,8 @@ CREATE TYPE etat_c AS ENUM('normal', 'renvoye' /*, … */);
 
 CREATE TABLE personne(
 
-                  prenom VARCHAR,
-                  nom VARCHAR,
+                  prenom VARCHAR(255),
+                  nom VARCHAR(255),
                   
                   /*
                     login = SCAC               pour le transporteur
@@ -14,36 +14,39 @@ CREATE TABLE personne(
 
                     le login est l'identifiant unique
                   */
-                  login VARCHAR UNIQUE,
-                  mot_de_passe VARCHAR,
+                  login VARCHAR UNIQUE NOT NULL,
+                  mot_de_passe VARCHAR(255) NOT NULL DEFAULT 'bd6',
                   
-                  type_personne type_p,
+                  type_personne type_p NOT NULL,
 
                   PRIMARY KEY(login)
 );
 
 CREATE TABLE douane(
   
-                  id VARCHAR UNIQUE,
+                  id VARCHAR(255) UNIQUE NOT NULL,
 
-                  pays VARCHAR,
+                  pays VARCHAR(255) NOT NULL,
 
                   FOREIGN KEY(id) REFERENCES personne(login)
 );
 
 CREATE TABLE client(
 
-                  id VARCHAR UNIQUE,
+                  id VARCHAR(255) UNIQUE NOT NULL,
                   
                   /* nom_societe VARCHAR,
                      suffixe_societe VARCHAR, -> fusionnés en 1 champ: personne.nom */
                   
-                  adresse VARCHAR,
-                  ville VARCHAR,
-                  code_postal VARCHAR,
-                  pays VARCHAR,
+                  adresse VARCHAR(255) NOT NULL,
+                  ville VARCHAR(255) NOT NULL,
+                  /* d'après Wikipedia, un code postal est <= 7 caractères, mais
+                     on laisse de la marge à cause des CP fantaisistes de
+                     data.csv */
+                  code_postal VARCHAR(10) NOT NULL,
+                  pays VARCHAR(255) NOT NULL,
                   
-                  telephone VARCHAR,
+                  telephone VARCHAR(255) NOT NULL,
 
                   FOREIGN KEY(id) REFERENCES personne(login)
 );
@@ -51,30 +54,30 @@ CREATE TABLE client(
 
 CREATE TABLE catalogue(
 
-                ref VARCHAR UNIQUE,
-                description VARCHAR,
-                qualifiant qualif,
+                ref VARCHAR UNIQUE NOT NULL,
+                description VARCHAR(255) DEFAULT '',
+                qualifiant qualif NOT NULL,
 
-                prix FLOAT,
-                poids FLOAT,
+                prix FLOAT CONSTRAINT prix_positif CHECK (prix >= 0),
+                poids FLOAT CONSTRAINT poids_positif CHECK (poids >= 0),
 
-                quantite_restante INTEGER,
+                quantite_restante INTEGER CONSTRAINT quantite_positive CHECK (quantite_restante >= 0) DEFAULT 0,
 
-                quantite_par_carton INTEGER,
-                cartons_par_palette INTEGER,
+                quantite_par_carton INTEGER CONSTRAINT quantite_par_carton_positive CHECK (quantite_par_carton > 0),
+                cartons_par_palette INTEGER CONSTRAINT cartons_par_palette_positif CHECK (cartons_par_palette > 0),
 
                 PRIMARY KEY(ref)
 );
 
 CREATE TABLE commande(
               
-                id SERIAL UNIQUE,
-                id_client VARCHAR,
+                id SERIAL UNIQUE NOT NULL,
+                id_client VARCHAR NOT NULL,
 
-                date_commande DATE,
+                date_commande DATE NOT NULL,
                 date_prevue DATE,
 
-                frais FLOAT,
+                frais FLOAT CONSTRAINT frais_positifs CHECK (frais >= 0),
 
                 FOREIGN KEY(id_client) REFERENCES client(id),
                 PRIMARY KEY(id)
@@ -82,9 +85,9 @@ CREATE TABLE commande(
 
 CREATE TABLE commande_produits(
 
-                id_commande INTEGER,
-                ref_produit VARCHAR,
-                quantite INTEGER,
+                id_commande INTEGER NOT NULL,
+                ref_produit VARCHAR NOT NULL,
+                quantite INTEGER CONSTRAINT quantite_positive CHECK (quantite > 0),
 
                 FOREIGN KEY(id_commande) REFERENCES commande(id),
                 FOREIGN KEY(ref_produit) REFERENCES catalogue(ref)
@@ -92,15 +95,15 @@ CREATE TABLE commande_produits(
 
 CREATE TABLE colis(
 
-                id SERIAL UNIQUE,
+                id SERIAL UNIQUE NOT NULL,
 
-                date_emballage DATE,
+                date_emballage DATE NOT NULL,
                 date_expedie DATE,
                 date_livraison DATE,
 
-                etat etat_c,
+                etat etat_c NOT NULL,
 
-                id_commande INTEGER,
+                id_commande INTEGER NOT NULL,
 
                 PRIMARY KEY(id),
                 FOREIGN KEY(id_commande) REFERENCES commande(id)
@@ -108,16 +111,16 @@ CREATE TABLE colis(
 
 CREATE TABLE colis_produits(
 
-                id_colis INTEGER UNIQUE,
-                ref_produit VARCHAR,
-                quantite INTEGER,
+                id_colis INTEGER UNIQUE NOT NULL,
+                ref_produit VARCHAR(255) NOT NULL,
+                quantite INTEGER CONSTRAINT quantite_positive CHECK (quantite > 0),
 
                 FOREIGN KEY(id_colis) REFERENCES colis(id),
                 FOREIGN KEY(ref_produit) REFERENCES catalogue(ref)
 );
 
 CREATE TABLE palette(
-                id INTEGER UNIQUE,
+                id INTEGER UNIQUE NOT NULL,
                 /* … */
 
                 PRIMARY KEY(id)
@@ -125,8 +128,8 @@ CREATE TABLE palette(
 
 CREATE TABLE palette_colis(
                 
-                id_palette INTEGER,
-                id_colis INTEGER,
+                id_palette INTEGER NOT NULL,
+                id_colis INTEGER NOT NULL,
 
                 FOREIGN KEY(id_palette) REFERENCES palette(id),
                 FOREIGN KEY(id_colis) REFERENCES colis(id)
@@ -134,9 +137,9 @@ CREATE TABLE palette_colis(
 
 CREATE TABLE container(
 
-                id INTEGER UNIQUE,
-                id_transporteur VARCHAR,
-                id_emballeur VARCHAR,
+                id INTEGER UNIQUE NOT NULL,
+                id_transporteur VARCHAR(255) NOT NULL,
+                id_emballeur VARCHAR(255) NOT NULL,
 
                 FOREIGN KEY(id_transporteur) REFERENCES personne(login),
                 FOREIGN KEY(id_emballeur) REFERENCES personne(login),
@@ -145,8 +148,8 @@ CREATE TABLE container(
 
 CREATE TABLE container_palettes(
 
-                id_container INTEGER,
-                id_palette INTEGER,
+                id_container INTEGER NOT NULL,
+                id_palette INTEGER NOT NULL,
 
                 FOREIGN KEY(id_container) REFERENCES container(id),
                 FOREIGN KEY(id_palette) REFERENCES palette(id)
