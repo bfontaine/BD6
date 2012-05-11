aeaf84cf594e90ec29749dfef5fa0bdd35a06372
david <galichet.david@gmail.com>
2012-05-11 20:50:20 +0200

    erreur

--- java/Interface_texte.java
+++ java/Interface_texte.java
@@ -8,12 +8,12 @@ import java.util.LinkedList;
 public class Interface_texte{
     static ConnexionBDD co;
     static Scanner in = new Scanner(System.in);
-    
+
     /**
-    * Imprime le menu de connection
-    **/
+     * Imprime le menu de connection
+     **/
     public static void menuConnexion(){
-       
+
         System.out.print("\033c"); //nettoyage de l'ecran
         String type;
         String login;
@@ -39,179 +39,182 @@ public class Interface_texte{
         if (type.equals("client")) {
             while(choix != 4 ){
                 choix = menuClient(login);
-                switch(choix){
-                    case 0: break;
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
-                    case 4: break;
+                if((choix < 4) || (choix > -1)){
+                    choix_client(choix);
                 }
             }
-                
+
         }
         else if (type.equals("transporteur")) {
             while(choix != 3 ){
                 choix = menuTransporteur(login);
-                switch(choix){
-                    case 0: break;
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
+                if((choix < 2) || (choix > -1)){
+                    choix_client(choix);
                 }
             }
         }
         else if (type.equals("emballeur")) {
             while(choix != 3 ){
                 choix = menuEmballeur(login);
-                switch(choix){
-                    case 0: break;
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
+                if((choix < 2) || (choix > -1)){
+                    choix_client(choix);
                 }
             }
         }
         else if (type.equals("gerant")) {
             while(choix != 5 ){
                 choix = menuGerant(login);
-                switch(choix){
-                    case 0: break;
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
-                    case 4: break;
-                    case 5: break;
+                if((choix < 4) || (choix > -1)){
+                    choix_client(choix);
                 }
             }
         }
         else if (type.equals("douane")) {
             while(choix != 5 ){
                 choix = menuDouane(login);
-                switch(choix){
-                    case 0: break;
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
-                    case 4: break;
-                    case 5: break;
+                if((choix < 4) || (choix > -1)){
+                    choix_client(choix);
                 }
             }
         }
     }
 
     public static int menuClient(String login){
-            System.out.print("\033c"); //nettoyage de l'ecran
+        System.out.print("\033c"); //nettoyage de l'ecran
 
-            // -------------------
-            // Impression du menu   
-            // -------------------
+        // -------------------
+        // Impression du menu   
+        // -------------------
 
-            System.out.println("Veuillez entrer votre choix :");
-            System.out.println("-------------------------------------------------------------");
-            System.out.println("0 - Passer une commande");
-            System.out.println("1 - Situation commande"); 
-            System.out.println("2 - Lister les produits disponibles");
-            System.out.println("3 - Changer son login/mdp");
-            System.out.println("4 - quitter");
-            System.out.println("-------------------------------------------------------------");
-            System.out.print("choix :");
+        System.out.println("Veuillez entrer votre choix :");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("0 - Passer une commande");
+        System.out.println("1 - Situation commande"); 
+        System.out.println("2 - Lister les produits disponibles");
+        System.out.println("3 - Changer son login/mdp");
+        System.out.println("4 - quitter");
+        System.out.println("-------------------------------------------------------------");
+        System.out.print("choix :");
 
-            return in.nextInt();
+        return in.nextInt();
     }
 
     public static int menuGerant(String login){
-            System.out.print("\033c"); //nettoyage de l'ecran
+        System.out.print("\033c"); //nettoyage de l'ecran
 
-            // -------------------
-            // Impression du menu   
-            // -------------------
+        // -------------------
+        // Impression du menu   
+        // -------------------
 
-            System.out.println("Veuillez entrer votre choix :");
-            System.out.println("-------------------------------------------------------------");
-            System.out.println("0 - A accès à la liste des employés/clients");
-            System.out.println("1 - Changer le prix des produits"); 
-            System.out.println("2 - Voir les produits les plus vendus");
-            System.out.println("3 - Voir les clients les plus depensiés");
-            System.out.println("4 - Voir les employés les moins actifs");
-            System.out.println("5 - quitter");
-            System.out.println("-------------------------------------------------------------");
-            System.out.print("choix :");
+        System.out.println("Veuillez entrer votre choix :");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("0 - A accès à la liste des employés/clients");
+        System.out.println("1 - Changer le prix des produits"); 
+        System.out.println("2 - Voir les produits les plus vendus");
+        System.out.println("3 - Voir les clients les plus depensiés");
+        System.out.println("4 - Voir les employés les moins actifs");
+        System.out.println("5 - quitter");
+        System.out.println("-------------------------------------------------------------");
+        System.out.print("choix :");
 
-            return in.nextInt();
+        return in.nextInt();
     }
 
     public static int  menuTransporteur(String login){
-            System.out.print("\033c"); //nettoyage de l'ecran
+        System.out.print("\033c"); //nettoyage de l'ecran
 
-            // -------------------
-            // Impression du menu   
-            // -------------------
+        // -------------------
+        // Impression du menu   
+        // -------------------
 
-            System.out.println("Veuillez entrer votre choix :");
-            System.out.println("-------------------------------------------------------------");
-            System.out.println("0 - Connaître la liste des commandes d'un client");
-            System.out.println("1 - Entrer les colis emballés"); 
-            System.out.println("2 - Entrer les palettes préparées");
-            System.out.println("3 - quitter");
-            System.out.println("-------------------------------------------------------------");
-            System.out.print("choix :");
+        System.out.println("Veuillez entrer votre choix :");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("0 - Connaître la liste des commandes d'un client");
+        System.out.println("1 - Entrer les colis emballés"); 
+        System.out.println("2 - Entrer les palettes préparées");
+        System.out.println("3 - quitter");
+        System.out.println("-------------------------------------------------------------");
+        System.out.print("choix :");
 
-            return in.nextInt();
+        return in.nextInt();
     }
 
     public static int menuEmballeur(String login){
-            System.out.print("\033c"); //nettoyage de l'ecran
+        System.out.print("\033c"); //nettoyage de l'ecran
 
-            // -------------------
-            // Impression du menu   
-            // -------------------
+        // -------------------
+        // Impression du menu   
+        // -------------------
 
-            System.out.println("Veuillez entrer votre choix :");
-            System.out.println("-------------------------------------------------------------");
-            System.out.println("0 - Connaître la date limite de livraison souhaitée par le client");
-            System.out.println("1 - Savoir si un colis est fragile ou dangereux"); 
-            System.out.println("2 - Changer situation du colis");
-            System.out.println("3 - quitter");
-            System.out.println("-------------------------------------------------------------");
-            System.out.print("choix :");
+        System.out.println("Veuillez entrer votre choix :");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("0 - Connaître la date limite de livraison souhaitée par le client");
+        System.out.println("1 - Savoir si un colis est fragile ou dangereux"); 
+        System.out.println("2 - Changer situation du colis");
+        System.out.println("3 - quitter");
+        System.out.println("-------------------------------------------------------------");
+        System.out.print("choix :");
 
-            return in.nextInt();
+        return in.nextInt();
     }
 
     public static int menuDouane(String login){
-            System.out.print("\033c"); //nettoyage de l'ecran
+        System.out.print("\033c"); //nettoyage de l'ecran
 
-            // -------------------
-            // Impression du menu   
-            // -------------------
+        // -------------------
+        // Impression du menu   
+        // -------------------
 
-            System.out.println("Veuillez entrer votre choix :");
-            System.out.println("-------------------------------------------------------------");
-            System.out.println("0 - ");
-            System.out.println("1 - "); 
-            System.out.println("2 - ");
-            System.out.println("3 - ");
-            System.out.println("4 - ");
-            System.out.println("5 - quitter");
-            System.out.println("-------------------------------------------------------------");
-            System.out.print("choix :");
+        System.out.println("Veuillez entrer votre choix :");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("0 - ");
+        System.out.println("1 - "); 
+        System.out.println("2 - ");
+        System.out.println("3 - ");
+        System.out.println("4 - ");
+        System.out.println("5 - quitter");
+        System.out.println("-------------------------------------------------------------");
+        System.out.print("choix :");
 
-            return in.nextInt();
+        return in.nextInt();
     }
 
-    public static void choix_client(){
+    public static void choix_client(int choix){
+        if(choix == 0){
 
+        }
+        else if(choix == 1){
+
+        }
+        else if(choix == 2){
+            int [] taille = {16,50,10,7,4,5};
+            String[] champ = {"référence","description","qualifiant","prix","poids","quantité restante"};
+
+            affichage_less(co.listeProduitsRestants(),champ,taille,"Liste des Produit disponible :");
+        }
+        else if(choix == 3){
+
+        }
     }
 
 
-    public void affichage_less(LinkedList<HashMap<String,Object>> liste,String[] champ,int[] taille){
-        
+    public static void affichage_less (LinkedList<HashMap<String,Object>> liste, String[] champ,int[] taille, String nom req){
         int choix = -1;
         int ligne = 0;
         int total = 0;
         while(choix != 0){
-            System.out.println("Affichage :");
+            System.out.print("\033c"); //nettoyage de l'ecran
+            System.out.println(req);
             System.out.println("-------------------------------------------------------------");
+            //affiche les nom de colonne
+            for(int i = 0; i < champ.length; i++){
+                String mot = champ[i];
+                for ( int j = mot.length(); j < taille[i] ;j++){
+                    mot += " ";
+                }
+                System.out.print(mot+"| ");
+            }
+            //affiche le contenu du LinkedListe
             do{
                 HashMap<String,Object> hb = liste.get(total);
                 for(int i = 0; i < champ.length; i++){
